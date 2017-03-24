b7d0a85143358a754de5d1482676cf7ebccb81c2
wakingrufus <wakingrufus@gmail.com>
2017-03-24 17:05:11 -0500

    initital commit

---
+++ src/main/java/com/github/wakingrufus/microblog/Main.java
@@ -0,0 +1,10 @@
+package com.github.wakingrufus.microblog;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args){
+        log.info("test");
+    }
+}
